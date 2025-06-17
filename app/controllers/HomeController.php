89ea47294a343eb9f0db76c552b640f7456c461d
svacant <svacant@gmail.com>
2025-06-17 14:51:27 +0200

    Add lightweight templating engine

---
+++ app/controllers/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . '/../core/View.php';
+require_once __DIR__ . '/../../data.php';
+
+class HomeController
+{
+    public function index(): void
+    {
+        global $data, $shoppiPageId;
+
+        $sliderPath = './fotos/slider/';
+        $sliderFiles = array_values(array_diff(scandir($sliderPath), ['.', '..']));
+
+        $brandPath = './fotos/marken/';
+        $brandFiles = [];
+        if (is_dir($brandPath)) {
+            $brandFiles = array_values(array_diff(scandir($brandPath), ['.', '..']));
+        }
+
+        View::render('home', [
+            'data'        => $data,
+            'shoppiPageId'=> $shoppiPageId,
+            'sliderFiles' => $sliderFiles,
+            'brands'      => $brandFiles,
+            'year'        => date('Y')
+        ]);
+    }
+}
